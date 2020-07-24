1a805ac45f7aeaed7d571855bb7f01edc885709c
liudezhi <a.liudezhi@gmail.com>
2020-07-24 23:29:43 +0800

    -

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root "topics#index"
+    root "topics#index"
   resources :topics do
     member do
       post 'upvote'
+      post 'downvote'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
